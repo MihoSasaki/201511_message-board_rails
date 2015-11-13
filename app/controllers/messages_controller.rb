4e41ac44f5fb5844aa9c41233c229fbee809fc12
mihosasaki <miho.sprit@gmail.com>
2015-11-13 13:12:13 +0000

    bootstrap-edited

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -7,10 +7,9 @@ class MessagesController < ApplicationController
      @message.save
      redirect_to root_path , notice: 'メッセージを保存しました'
   end
-  
+    
   private
   def message_params
     params.require(:message).permit(:name, :body)
   end
 end
-
