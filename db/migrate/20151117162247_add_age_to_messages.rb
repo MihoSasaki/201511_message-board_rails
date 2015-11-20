0d4fe800a18bdb78f1f165bb91e4146d1843564d
mihosasaki <miho.sprit@gmail.com>
2015-11-20 12:47:28 +0000

    update

--- db/migrate/20151117162247_add_age_to_messages.rb
+++ db/migrate/20151117162247_add_age_to_messages.rb
@@ -1,8 +1,6 @@
 class AddAgeToMessages < ActiveRecord::Migration
   def change
     add_column :messages, :age, :integer
-    change_table messages do |t|
-      t.integer :age, :integer, :scale => false
-    end
+    
   end
 end
