3d0b6009680197e3df97ea9fa1d3c512ac303830
mihosasaki <miho.sprit@gmail.com>
2015-11-20 12:52:45 +0000

    updated

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,6 +1,6 @@
 class Message < ActiveRecord::Base
   validates :name , length: { maximum: 20 } , presence:true
   validates :body , length: { minimum: 2 } , presence:true
-  validates :age , numericality: {only_integer: true, greater_than:0 } , presence:true
+  validates :age , numericality: {only_integer: true, greater_than_or_equal_to:0 } , presence:true
   
 end
